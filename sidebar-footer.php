3aa32fd1b00963dad930794671bd44d36d45c58c
sarah <sarah@triggersandsparks.com>
2015-12-21 17:15:20 +0000

    Add breakpoint mixins and adjust grid for better responsivity.

---
+++ sidebar-footer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The footer area containing three widget areas.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Feminist_Frequency
+ */
+
+ if ( is_active_sidebar( 'footer-1' ) || is_active_sidebar( 'footer-2' ) || is_active_sidebar( 'footer-3' ) ) : ?>
+ 	<div id="tertiary" class="footer-widget-area" role="complementary">
+ 		<?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
+ 		    <div id="footer-1" class="widget-area">
+ 		        <?php dynamic_sidebar( 'footer-1' ); ?>
+ 		    </div>
+ 		<?php endif; ?>
+
+ 		<?php if ( is_active_sidebar( 'footer-2' ) ) : ?>
+ 		    <div id="footer-2" class="widget-area">
+ 		        <?php dynamic_sidebar( 'footer-2' ); ?>
+ 		    </div>
+ 		<?php endif; ?>
+
+ 		<?php if ( is_active_sidebar( 'footer-3' ) ) : ?>
+ 		    <div id="footer-3" class="widget-area">
+ 		        <?php dynamic_sidebar( 'footer-3' ); ?>
+ 		    </div>
+ 		<?php endif; ?>
+ 	</div><!-- #tertiary -->
+<?php endif;
