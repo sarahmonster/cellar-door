0c481725874796f080db67493d25c6ee9ce793e8
sarah <sarah@triggersandsparks.com>
2015-12-22 01:39:50 +0000

    Update JS file locations to reflect actual hrefs.

--- functions.php
+++ functions.php
@@ -152,9 +152,9 @@ add_action( 'widgets_init', 'femfreq_widgets_init' );
 function femfreq_scripts() {
 	wp_enqueue_style( 'femfreq-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'femfreq-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	wp_enqueue_script( 'femfreq-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20120206', true );
 
-	wp_enqueue_script( 'femfreq-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'femfreq-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
