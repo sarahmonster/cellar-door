01d0456af0407dbce95ad96c29accbfb7426776c
sarah <sarah@triggersandsparks.com>
2016-01-06 20:22:45 +0000

    Remove link underline and data-hover attribute for all image links.

--- assets/js/femfreq.js
+++ assets/js/femfreq.js
@@ -9,7 +9,12 @@
 
 	// Add a data-atttribute to all .entry-content links
 	$( '.entry-content' ).find( 'a' ).each( function() {
-		$( this ).attr( 'data-hover', $( this ).text() );
+		if ( ! $( this ) ).has( 'img' ) ) {
+			$( this ).attr( 'data-hover', $( this ).text() );
+		}
 	} );
+
+	// Add a class to all links that contain images
+	$( '.entry-content' ).find( 'a' ).has( 'img' ).addClass( 'image-link' );
 
 } )( jQuery );
