dbef30c69a5533e46921248688eefa0e1d543e7f
sarah <sarah@triggersandsparks.com>
2016-01-06 21:46:25 +0000

    Give post images a little style.

--- assets/js/femfreq.js
+++ assets/js/femfreq.js
@@ -7,14 +7,31 @@
 
 ( function( $ ) {
 
-	// Add a data-atttribute to all .entry-content links
-	$( '.entry-content' ).find( 'a' ).each( function() {
-		if ( ! $( this ) ).has( 'img' ) ) {
-			$( this ).attr( 'data-hover', $( this ).text() );
-		}
+	// Run our functions once the window has loaded fully
+	$( window ).on( 'load', function() {
+
+		// Add a data-atttribute to all .entry-content links
+		$( '.entry-content' ).find( 'a' ).each( function() {
+			if ( 0 < $( this ).has( 'img' ).length ) {
+				$( this ).attr( 'rel', 'attachment' );
+			} else {
+				$( this ).attr( 'data-hover', $( this ).text() );
+			}
+		} );
+
+		// Add a class to all links that contain images
+		//$( '.entry-content' ).find( 'a' ).has( 'img' ).addClass( 'image-link' );
+
+		// Remove height and width attributes from images
+		$( '.entry-content' ).find( 'img' ).each( function() {
+			if ( ! $( this ).hasClass( 'aligncenter' ) ) {
+				$( this ).removeAttr( 'width' )
+				$( this ).removeAttr( 'height' );
+				$( this ).removeAttr( 'sizes' );
+			}
+		} );
+
 	} );
 
-	// Add a class to all links that contain images
-	$( '.entry-content' ).find( 'a' ).has( 'img' ).addClass( 'image-link' );
 
 } )( jQuery );
