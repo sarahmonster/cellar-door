cab99544424bebaa9f23047356dc052c04c2f8f2
sarah <sarah@triggersandsparks.com>
2016-01-12 17:42:14 +0000

    Style post navigation.

--- single.php
+++ single.php
@@ -17,7 +17,10 @@ get_header(); ?>
 
 			get_template_part( 'template-parts/content-single', get_post_format() );
 
-			the_post_navigation();
+			the_post_navigation( array(
+						'prev_text' => femfreq_get_icon( 'arrow' ) . '<span>' . esc_html__( 'Previous', 'femfreq' ) . '</span> %title',
+						'next_text' => femfreq_get_icon( 'arrow' ) . '<span>' . esc_html__( 'Next', 'femfreq' ) . '</span> %title',
+			) );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
