698cf3a5bfb747464783927fe851e32239af1a56
sarah <sarah@triggersandsparks.com>
2016-01-19 12:56:36 +0100

    Fix indentation.

--- inc/metadata.php
+++ inc/metadata.php
@@ -6,14 +6,14 @@
  * @package Feminist_Frequency
  */
 
- add_action( 'fm_post_post', function() {
-     $fm = new Fieldmanager_Group( array(
-         'name' => 'game_info',
-         'children' => array(
-             'name' => new Fieldmanager_Textfield( __( 'Year Published', 'femfreq' ) ),
-             'phone_number' => new Fieldmanager_Textfield( __( 'Developer', 'femfreq' ) ),
-             'website' => new Fieldmanager_Link( __( 'Publisher', 'femfreq' ) ),
-         ),
-     ) );
-     $fm->add_meta_box( __( 'Game Details', 'femfreq' ), 'post' );
- } );
+add_action( 'fm_post_post', function() {
+    $fm = new Fieldmanager_Group( array(
+        'name' => 'game_info',
+        'children' => array(
+            'name' => new Fieldmanager_Textfield( __( 'Year Published', 'femfreq' ) ),
+            'phone_number' => new Fieldmanager_Textfield( __( 'Developer', 'femfreq' ) ),
+            'website' => new Fieldmanager_Link( __( 'Publisher', 'femfreq' ) ),
+        ),
+    ) );
+    $fm->add_meta_box( __( 'Game Details', 'femfreq' ), 'post' );
+} );
