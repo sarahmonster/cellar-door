107c0a023100061b8722c9021e7066ba704d24ba
sarah <sarah@triggersandsparks.com>
2016-01-19 13:26:42 +0100

    Add game information to top of sidebar.

--- inc/metadata.php
+++ inc/metadata.php
@@ -8,12 +8,52 @@
 
 add_action( 'fm_post_post', function() {
     $fm = new Fieldmanager_Group( array(
-        'name' => 'game_info',
+        'name' => 'game_information',
         'children' => array(
-            'name' => new Fieldmanager_Textfield( __( 'Year Published', 'femfreq' ) ),
-            'phone_number' => new Fieldmanager_Textfield( __( 'Developer', 'femfreq' ) ),
-            'website' => new Fieldmanager_Link( __( 'Publisher', 'femfreq' ) ),
+            'date_published' => new Fieldmanager_Textfield( __( 'Date Published', 'femfreq' ) ),
+            'developer' => new Fieldmanager_Textfield( __( 'Developer', 'femfreq' ) ),
+            'publisher' => new Fieldmanager_Textfield( __( 'Publisher', 'femfreq' ) ),
         ),
     ) );
-    $fm->add_meta_box( __( 'Game Details', 'femfreq' ), 'post' );
+    $fm->add_meta_box( __( 'Game Information', 'femfreq' ), 'post' );
 } );
+
+/**
+ * Retrieve game information and display it.
+ */
+function femfreq_game_information( $id ) {
+    $game_information = get_post_meta( $id, 'game_information' );
+    if ( 0 < count( $game_information ) ) :
+        $game = $game_information[0];
+
+        echo '<h2 class="widget-title">';
+        esc_html_e( 'Game information', 'femfreq' );
+        echo '</h2>';
+
+        echo '<ul>';
+
+        if ( $game['date_published'] ) :
+            echo '<li>';
+            esc_html_e( 'Published: ', 'femfreq' );
+            echo $game['date_published'];
+            echo '</li>';
+        endif;
+
+        if ( $game['developer'] ) :
+            echo '<li>';
+            esc_html_e( 'Developer: ', 'femfreq' );
+            echo $game['developer'];
+            echo '</li>';
+        endif;
+
+        if ( $game['publisher'] ) :
+            echo '<li>';
+            esc_html_e( 'Publisher: ', 'femfreq' );
+            echo $game['publisher'];
+            echo '</li>';
+        endif;
+
+        echo '</ul>';
+
+    endif;
+}
