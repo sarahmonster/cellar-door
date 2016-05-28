94b99a22484e6361bb9a1b49ec9f9c7be6498a87
sarah <sarah@triggersandsparks.com>
2016-05-28 02:28:07 -0700

    Remove game information from sidebar.

--- sidebar.php
+++ sidebar.php
@@ -9,7 +9,6 @@
 
 if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 	<aside id="secondary" class="widget-area" role="complementary">
-		<?php cellardoor_game_information( get_the_ID() ); ?>
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
 	</aside><!-- #secondary -->
 <?php endif;
